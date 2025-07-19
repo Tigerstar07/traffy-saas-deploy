8666d80afd023f00ebcd4196ace0e8e41d0c4f7d
Tigerstar07 <roberts@lords.id.lv>
2025-07-19 15:11:42 +0300

    first good look

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,16 +1,16 @@
-const card = document.getElementById("traffyCard");
+const text = document.getElementById("traffy");
 
-card.addEventListener("mousemove", (e) => {
-    const rect = card.getBoundingClientRect();
-    const x = (e.clientX - rect.left) / rect.width;
-    const y = (e.clientY - rect.top) / rect.height;
+text.addEventListener("mousemove", (e) => {
+  const rect = text.getBoundingClientRect();
+  const x = (e.clientX - rect.left) / rect.width;
+  const y = (e.clientY - rect.top) / rect.height;
 
-    const rotateX = (0.5 - y) * 15;
-    const rotateY = (x - 0.5) * 15;
+  const rotateX = (0.5 - y) * 15;
+  const rotateY = (x - 0.5) * 15;
 
-    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.04)`;
+  text.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.04)`;
 });
 
-card.addEventListener("mouseleave", () => {
-    card.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
+text.addEventListener("mouseleave", () => {
+  text.style.transform = `rotateX(0deg) rotateY(0deg) scale(1)`;
 });
