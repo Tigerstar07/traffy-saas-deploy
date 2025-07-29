a6fb16ff7f9cdb9de88a40fd467ac17b3c746946
Matainais <mareks@lords.id.lv>
2025-07-29 09:55:59 +0300

    DB pieslēgšana

---
+++ php/includes/db.php
@@ -0,0 +1,25 @@
+<?php 
+  
+  $db['db_host'] = 'localhost';
+  $db['db_user'] = 'traffylv_SAAS';
+  $db['db_pass'] = '?zr;FuL*!e*}K7*L';
+  $db['db_name'] = 'traffylv_SAAS';
+
+  foreach($db as $key => $value){
+
+    define(strtoupper($key), $value);
+
+  }
+
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+mysqli_set_charset($connection,"utf8mb3");
+
+    if(!$connection) {
+
+        die("Databse connection failed!" . mysqly_error($connection));
+
+    }
+
+?>
