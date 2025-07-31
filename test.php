a2cc4bb0a908f626c32b4987d8cf7c1e87f01517
Matainais <mareks@lords.id.lv>
2025-07-31 09:13:50 +0300

    tr

--- test.php
+++ test.php
@@ -15,18 +15,18 @@ register_user();
 
 <form id="signupForm" action="" method="post">
     <label for="c_name">Company name</label><br>
-    <input type="text" id="c_name" placeholder="SIA Piemērs" name="c_name"/>
+    <input type="text" id="c_name" placeholder="SIA Piemērs" name="c_name"/><br>
     <label for="c_r_number">Registration number</label><br>
-    <input type="text" id="c_r_number" onKeyPress="if(this.value.length==11) return false;" placeholder="00000000000" name="c_r_number"/>
+    <input type="text" id="c_r_number" onKeyPress="if(this.value.length==11) return false;" placeholder="00000000000" name="c_r_number"/><br>
     <label for="first">First name</label></br>
-    <input type="text" id="first" placeholder="Jānis" name="first"/>
+    <input type="text" id="first" placeholder="Jānis" name="first"/><br>
     <label for="last">Last name</label></br>
-    <input type="text" id="last" placeholder="Ozoliņš" name="last"/>
+    <input type="text" id="last" placeholder="Ozoliņš" name="last"/><br>
     <label for="email">Email Address</label></br>
-    <input type="email" id="email" placeholder="piemers@piemers.lv" required name="email"/>
+    <input type="email" id="email" placeholder="piemers@piemers.lv" required name="email"/><br>
     <label for="password">Password</label></br>
-    <input type="password" id="password" placeholder="••••••••" required name="password"/>
-    <input class="form-check-input" type="checkbox" value="yes" id="commercials" name="news"></br>
+    <input type="password" id="password" placeholder="••••••••" required name="password"/><br>
+    <input class="form-check-input" type="checkbox" value="yes" id="commercials" name="news">
     <label class="form-check-label" for="ligumaNoteikumi">Vēlos saņemt jaunumus epastā.</label></br>
     <button type="submit" name="register">Sign up →</button>
 </form>
