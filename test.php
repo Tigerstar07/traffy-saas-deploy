eaba89423fe3a409c7ed98124b5f34f5aa6de660
Matainais <mareks@lords.id.lv>
2025-07-31 09:16:55 +0300

    tr

--- test.php
+++ test.php
@@ -14,17 +14,17 @@ register_user();
 <h2>Testa forma</h2>
 
 <form id="signupForm" action="" method="post">
-    <label for="c_name">Company name</label><br>
+    <label for="c_name">Kompānijas nosaukums</label><br>
     <input type="text" id="c_name" placeholder="SIA Piemērs" name="c_name"/><br>
-    <label for="c_r_number">Registration number</label><br>
+    <label for="c_r_number">Reģistrācijas numurs</label><br>
     <input type="text" id="c_r_number" onKeyPress="if(this.value.length==11) return false;" placeholder="00000000000" name="c_r_number"/><br>
-    <label for="first">First name</label></br>
+    <label for="first">Vārds</label></br>
     <input type="text" id="first" placeholder="Jānis" name="first"/><br>
-    <label for="last">Last name</label></br>
+    <label for="last">Uzvārds</label></br>
     <input type="text" id="last" placeholder="Ozoliņš" name="last"/><br>
-    <label for="email">Email Address</label></br>
+    <label for="email">E-pasta adrese</label></br>
     <input type="email" id="email" placeholder="piemers@piemers.lv" required name="email"/><br>
-    <label for="password">Password</label></br>
+    <label for="password">Parole</label></br>
     <input type="password" id="password" placeholder="••••••••" required name="password"/><br>
     <input class="form-check-input" type="checkbox" value="yes" id="commercials" name="news">
     <label class="form-check-label" for="ligumaNoteikumi">Vēlos saņemt jaunumus epastā.</label></br>
